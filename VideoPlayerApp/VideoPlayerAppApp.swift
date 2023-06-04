29d7fd9a10442415ca3615862fcb7efca49a8417
githubuiux <uiuxista@gmail.com>
2023-06-04 14:08:51 +0300

    Initial Commit

---
+++ VideoPlayerApp/VideoPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerAppApp.swift
+//  VideoPlayerApp
+//
+//  Created by Levent on 4.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
