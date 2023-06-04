a61240fa59a7cc9f54f2bfcb8c6cc0e33c96ce6f
githubuiux <uiuxista@gmail.com>
2023-06-04 15:55:59 +0300

    geliştirme

--- VideoPlayerApp/ContentView.swift
+++ VideoPlayerApp/ContentView.swift
@@ -6,16 +6,97 @@
 //
 
 import SwiftUI
+import AVKit
+import AVFoundation
 
 struct ContentView: View {
+    @State private var currentVideoIndex: Int
+    @State private var player: AVPlayer
+    @State private var playerList: [AVPlayer] = []
+    
+    let videoURLs = [
+        
+        URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!,
+        URL(string: "https://freetestdata.com/wp-content/uploads/2022/02/Free_Test_Data_7MB_MP4.mp4")!,
+        URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4")!
+    ]
+    
+    init() {
+        currentVideoIndex = 0
+        player = AVPlayer(url: videoURLs[0])
+        playerList.append(AVPlayer(url: videoURLs[0]))
+        playerList.append(AVPlayer(url: videoURLs[1]))
+        playerList.append(AVPlayer(url: videoURLs[2]))
+    }
+    
+    func setupPlayer(index: Int) {
+        player = playerList[index]
+        player.volume = 20
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VideoPlayer(player: player){
+            VStack{
+                Spacer()
+                Spacer()
+                Spacer()
+                HStack(spacing: 20){
+                    Image(uiImage: UIImage(named: "profile")!)
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .foregroundColor(.white)
+                        .cornerRadius(30)
+                    Text("@VideoWatchLikeYouTube")
+                        .foregroundColor(.white)
+                        .font(.custom("AmericanTypewriter", fixedSize: 12))
+                    
+                    Button("Subscribe") {
+                        
+                    }.padding(.all, 5)
+                        .background(Color.white)
+                        .cornerRadius(20)
+                        .foregroundColor(.black)
+                        .tint(.white)
+                }
+                .padding(.bottom, 10)
+                Text("A Composition of different sample videos shown as a test videos #Video # Testign")
+                    .foregroundColor(.white)
+                    .font(.custom("AmericanTypewriter", fixedSize: 15))
+                    .padding(.bottom, 10)
+
+                HStack {
+                    Image(systemName: "music.note")
+                        .resizable()
+                        .frame(width: 10, height: 10)
+                        .foregroundColor(.white)
+                        .padding(.leading, 20)
+                    Text("Orignial sound")
+                        .foregroundColor(.white)
+                        .font(.custom("AmericanTypewriter", fixedSize: 10))
+                    Spacer()
+                }
+                .padding(.bottom, 100)
+            }
+            .padding()
+            
+        }.gesture(DragGesture(minimumDistance: 20, coordinateSpace: .local)
+            .onEnded { gesture in
+                player.volume = 0.0
+                if gesture.translation.width < 0 {
+                    // swiped left, play next video
+                    currentVideoIndex = (currentVideoIndex+1)%videoURLs.count
+                } else if gesture.translation.width > 0 {
+                    // swipe right, play previous video
+                    currentVideoIndex = (currentVideoIndex-1 + videoURLs.count) % videoURLs.count
+                }
+                setupPlayer(index: currentVideoIndex)
+            })
+        .edgesIgnoringSafeArea(.all)
+        .onAppear{
+            playerList.append(AVPlayer(url: videoURLs[0]))
+            playerList.append(AVPlayer(url: videoURLs[1]))
+            playerList.append(AVPlayer(url: videoURLs[2]))
         }
-        .padding()
     }
 }
 
